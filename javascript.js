28d4c33ad0389334d683b1241286262856ca4375
nodetator <nodetator@gmail.com>
2022-11-08 23:40:29 +0800

    Code now plays one round of Rock Paper Scissors perfectly

--- javascript.js
+++ javascript.js
@@ -14,52 +14,77 @@ function getComputerChoice() {
 // A function that plays a single round of Rock, Paper Scissors
 // What is the logic here? Define Rock wins against Scissors, Paper wins against Rock etc
 
-function playRound (playerSelection, ComputerChoice) {
+function playRound (playerSelection, computerSelection) {
     // get User Choice
-    let Player = prompt("Enter Rock, Paper or Scissors").toLowerCase();
-    console.log(Player);
+    // let Player = prompt("Enter Rock, Paper or Scissors").toLowerCase();
+    //console.log(Player);
 
     // get Computer choice
-    let Computer = getComputerChoice();
-    console.log(Computer);
-
+    // let Computer = getComputerChoice();
+    // console.log(Computer);
     // Define the Logic 
+    if (playerSelection == computerSelection) {
+        return "It's a tie!!!";
 
-    if (Player == Computer) {
-        console.log("It's a tie!!!");
-
-    } else if (Player == 'rock') {
-        if (Computer == 'paper') {
-            console.log("Too bad!, you lost, Paper beats Rock!");
-        } else if (Computer == 'scissors') {
-            console.log("Congratulations, you won! Rock beats Scissors");
+    } else if (playerSelection == 'rock') {
+        if (computerSelection == 'paper') {
+            return "Too bad!, you lost, Paper beats Rock!";
+            
+        } else if (computerSelection == 'scissors') {
+            return "Congratulations, you won! Rock beats Scissors";
         }
-    } else if (Player == 'paper') {
-        if (Computer == 'rock') {
-            console.log("Congratulations, you won! Paper beats Rock!");
-        } else if (Computer == 'scissors') {
-            console.log("Too bad, you lost! Scissors beat paper");
+    } else if (playerSelection == 'paper') {
+        if (computerSelection== 'rock') {
+            return "Congratulations, you won! Paper beats Rock!";
+        } else if (computerSelection == 'scissors') {
+            return "Too bad, you lost! Scissors beat paper";
         }
 
-    } else if (Player == 'scissors') {
-        if (Computer == 'rock') {
-            console.log("Too bad, you lost! Rock beats scissors!");
-        } else if (Computer == 'paper') {
-            console.log("Congratulations, you won!, scissors beat paper");
+    } else if (playerSelection == 'scissors') {
+        if (computerSelection == 'rock') {
+            return "Too bad, you lost! Rock beats scissors!";
+        } else if (computerSelection == 'paper') {
+            return "Congratulations, you won!, scissors beat paper";
         }
-    }
-
+    } 
     
     }  
 
-console.log(playRound());
     // Rock beats scissors
     // Paper beats Rock
     // Scissors beat Paper;
     
+// Create a function toplay 5 rounds of Rock paper Scissors
+// Function should keep track of score
+// Function should report the results at the end
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        //inside this function, write code that plays 5 rounds
+        let playerSelection = prompt("Enter Rock, Paper or Scissors").toLowerCase();
+
+        let computerSelection = getComputerChoice();
+
+        playRound(playerSelection, computerSelection);
+        
+        //code above plays one round of Rock, Paper, Scissors
+
+        let compScore = 0;
+        let playerScore = 0;
+        //keeps track of the score
+        
+
+        if (result = "computer") {
+            compScore += 1;
+        } else if (result = "player") {
+            playerScore += 1;
+        }
+        // code above updates the score
 
 
 
-// Fourth, define a function that keeps score of a games best 3 out of 5
+        //prints the results of who won each round, and then the final score
+    }
+}
 
-
+game();
